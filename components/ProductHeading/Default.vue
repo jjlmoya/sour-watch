a4859ae60f666e4a6141e8fe958d23ad9489c40b
jjlmoya <jj.lopez@avoristravel.com>
2019-12-14 19:31:02 +0100

    Add price style with discount and functionality

--- components/ProductHeading/Default.vue
+++ components/ProductHeading/Default.vue
@@ -1,47 +1,56 @@
 <template>
     <div class="product-heading product-heading--default" :class="product.isNew ? 'is-new' : ''">
-        <Double :columns="'1-1'">
+        <Double :columns="'2-1'">
             <template slot="right">
-                <h1 class="product-heading__title">
-                    <div class="title">
-                        {{ product.collection }},
-                        <span class="f-strong">{{ product.model }}</span>
-                    </div>
-                    <div class="subtitle">
-                        ({{ product.brand }})
-                    </div>
-                </h1>
-                <div class="product-heading__details">
-                    <div class="price">
-                        {{ getCurrency(product.price) }}
-                    </div>
-                    <div class="availability">
-                        В наличии <span :class="product.stock === 0 ? 'error' : product.stock > 2 ? 'info' : 'warning' ">{{ product.stock }} часов </span>
-                    </div>
-                </div>
-                <div>{{ product.discount }}</div>
-                <div>{{ product.description }}</div>
-                <div>{{ product.gender }}</div>
-                <div>{{ product.mechanism }}</div>
-                <div>{{ product.calibre }}</div>
-                <div>{{ product.mechanismOrigin }}</div>
-                <div>{{ product.watchCase }}</div>
-                <div>{{ product.waterResistant }}</div>
-                <div>{{ product.width }}</div>
-                <div>{{ product.height }}</div>
-                <div>{{ product.thickness }}</div>
-                <div>{{ product.glass }}</div>
+                <div class="product-heading__info">
+                    <h1 class="product-heading__title">
+                        <div class="title">
+                            {{ product.collection }},
+                            <span class="f-strong">{{ product.model }}</span>
+                        </div>
+                        <div class="subtitle">
+                            ({{ product.brand }})
+                        </div>
+                    </h1>
+                    <div class="product-heading__details">
+                        <div v-if="product.discount" class="discount">
+                            -{{ product.discount }}%
+                        </div>
 
-                <div>
-                    <h2>Pulseras</h2>
-                    <div v-for="bracelet in product.bracelets" :key="bracelet.id">
-                        {{ bracelet.material }}
+                        <div class="price">
+                            <div v-if="product.discount" class="price--old">
+                                {{ getCurrency(product.price) }}
+                            </div>
+                            {{ getCurrency(getDiscountPrice(product.price, product.discount)) }}
+                        </div>
+                        <div class="availability">
+                            В наличии <span :class="product.stock === 0 ? 'error' : product.stock > 2 ? 'info' : 'warning' ">{{ product.stock }} часов </span>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <h2>Propiedades</h2>
-                    <div v-for="property in product.properties" :key="property.id">
-                        {{ property.name }} - {{ property.WatchProperty.value }}
+
+                    <div>{{ product.description }}</div>
+                    <div>{{ product.gender }}</div>
+                    <div>{{ product.mechanism }}</div>
+                    <div>{{ product.calibre }}</div>
+                    <div>{{ product.mechanismOrigin }}</div>
+                    <div>{{ product.watchCase }}</div>
+                    <div>{{ product.waterResistant }}</div>
+                    <div>{{ product.width }}</div>
+                    <div>{{ product.height }}</div>
+                    <div>{{ product.thickness }}</div>
+                    <div>{{ product.glass }}</div>
+
+                    <div>
+                        <h2>Pulseras</h2>
+                        <div v-for="bracelet in product.bracelets" :key="bracelet.id">
+                            {{ bracelet.material }}
+                        </div>
+                    </div>
+                    <div>
+                        <h2>Propiedades</h2>
+                        <div v-for="property in product.properties" :key="property.id">
+                            {{ property.name }} - {{ property.WatchProperty.value }}
+                        </div>
                     </div>
                 </div>
             </template>
@@ -78,6 +87,9 @@
         methods: {
             getCurrency (price) {
                 return currencyService.get(price)
+            },
+            getDiscountPrice (price, discount) {
+                return price - (price * (discount / 100))
             }
         }
     }
