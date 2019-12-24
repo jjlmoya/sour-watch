5060b1c2471b0080f7333dfb2c542f20b86444d5
jjlmoya <jj.lopez@avoristravel.com>
2019-12-24 19:08:41 +0300

    Add seo basic component

---
+++ components/Commons/Text.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="title || description" class="commons-text">
+        <div class="commons-text__title" :v-if="title">
+            {{ title }}
+        </div>
+        <div class="commons-text__description" :v-if="description">
+            {{ description }}
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+  @import 'commons-text.scss';
+</style>
+
+<script>
+    export default {
+        props: {
+            title: {
+                type: String,
+                default: ''
+            },
+            description: {
+                type: String,
+                default: ''
+            }
+        }
+    }
+</script>
