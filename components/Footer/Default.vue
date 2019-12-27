345e977c008f48a3890cb6bca95b7d766d25f098
jjlmoya <jj.lopez@avoristravel.com>
2019-12-27 22:18:27 +0300

    Add footer options

--- components/Footer/Default.vue
+++ components/Footer/Default.vue
@@ -2,17 +2,34 @@
     <footer class="footer footer--default">
         <div class="footer__wrapper">
             <div class="footer-static">
-                A
+                <a href="">Enlace 1</a>
+                <a href="">Enlace 2</a>
+                <a href="">Enlace 3</a>
+                <a href="">Enlace 4</a>
             </div>
             <div class="footer-links">
-                B
+                <a href="">Enlace 1</a>
+                <a href="">Enlace 2</a>
+                <a href="">Enlace 3</a>
+                <a href="">Enlace 4</a>
             </div>
             <div class="footer-contact">
-                C
+                <a href="">Enlace 1</a>
+                <a href="">Enlace 2</a>
+                <a href="">Enlace 3</a>
+                <a href="">Enlace 4</a>
+                <a href="">Enlace 1</a>
+                <a href="">Enlace 2</a>
+                <a href="">Enlace 3</a>
+                <a href="">Enlace 4</a>
+                <a href="">Enlace 1</a>
+                <a href="">Enlace 2</a>
+                <a href="">Enlace 3</a>
+                <a href="">Enlace 4</a>
             </div>
         </div>
-        <div class="footer__copywright">
-            © 2020 V. E. SL
+        <div class="footer__copyright">
+            © 2020 Vostok Europe Shop (Bonseo)
         </div>
     </footer>
 </template>
