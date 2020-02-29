c26ae9531c436aee7932f9e6a88303600b160974
jjlmoya <jj.lopez@avoristravel.com>
2020-02-29 18:30:00 +0300

    Inputs & Client Form

--- components/Forms/Client.vue
+++ components/Forms/Client.vue
@@ -1,23 +1,43 @@
 <template>
     <div class="forms-client">
-        <div class="">
-            <label>Nombre</label>
-            <input class="c-input" type="text" placeholder="Nombre">
-        </div>
-        <div class="">
-            <label>Email</label>
-            <input class="c-input" type="email" placeholder="Email">
-        </div>
-        <div class="">
-            <label>Phone</label>
-            <input class="c-input" type="phone" placeholder="Teléfoo">
+        <legend class="forms-client__legend">Datos personales</legend>
+        <div class="forms-client__fields">
+            <Input
+                :label="'Nombre:'"
+                :type="'text'"
+                :modifier="'name'"
+                :placeholder="'Nombre'"
+            />
+            <Input
+                :label="'Teléfono:'"
+                :type="'phone'"
+                :modifier="'phone'"
+                :placeholder="'Teléfono'"
+            />
+            <Input
+                :label="'Email:'"
+                :type="'email'"
+                :modifier="'email'"
+                :placeholder="'Email'"
+            />
+            <Input
+                :label="'Confirmar Email:'"
+                :type="'email'"
+                :modifier="'email'"
+                :placeholder="'Confirmar Email'"
+            />
         </div>
     </div>
 </template>
 <style lang="scss">@import 'client.scss';
 </style>
 <script>
+    import Input from '@/components/Forms/Input.vue'
+
     export default {
+        components: {
+            Input
+        },
         props: {
             image: {
                 type: String,
---
+++ components/Forms/Input.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="input" :class="modifier">
+        <label class="input__label" :class="`input__label--${type}`">{{ label }}</label>
+        <input class="input__element" :placeholder="placeholder" :class="`input__element--${type}`" :type="type">
+    </div>
+</template>
+
+<style lang="scss">@import 'input.scss';
+</style>
+
+<script>
+    export default {
+        props: {
+            modifier: {
+                type: String,
+                default: ''
+            },
+            type: {
+                type: String,
+                default: ''
+            },
+            label: {
+                type: String,
+                default: 'text'
+            },
+            placeholder: {
+                type: String,
+                default: ''
+            }
+        }
+    }
+</script>
