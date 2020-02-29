8a204eaa5d9bc3dfd60c9ce223b2731954a4f69c
jjlmoya <jj.lopez@avoristravel.com>
2020-02-29 17:06:45 +0300

    Add checkout middleware

--- middleware/product.js
+++ middleware/product.js
@@ -1,11 +1,16 @@
+import LoggerService from '@/services/logger.service'
+const loggerService = new LoggerService({ page: 'middleware', level: 'info' })
+
 export default async function ({ route, store }) {
     await store.dispatch('watches/get')
     await store.dispatch('collection/get')
+    loggerService.log(route.params.id)
     if (route && route.params && route.params.id) {
+        loggerService.log('Prueba')
         if (route.name === 'collekciya-id') {
             store.commit('collection/set', route.params.id)
         }
-        if (route.name === 'chasy-id') {
+        if (route.name === 'chasy-id' || route.name === 'checkout-id') {
             store.commit('watches/id', route.params.id)
         }
     }
